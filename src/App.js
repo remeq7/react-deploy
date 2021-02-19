e81681c8941ecba6dbaced0a845cafec29d2d46f
remeq7 <remremo20a@gmail.com>
2021-02-19 16:50:53 +0100

    react api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import "./index.css";
 
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [person, setPerson] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const item = data[2];
+    setPerson(item);
+    setLoading(false);
+  }, []);
+
+  fetch("https://course-api.com/react-tours-project")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
-    <div>
-      <h3>cos tam</h3>
-    </div>
+    <div>{loading ? <div>Loading...</div> : <div>{person.name}</div>}</div>
   );
 }
 
